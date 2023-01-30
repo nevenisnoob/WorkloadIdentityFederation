6b02dd8e86464f4d56e88c25157c3a49c36fa55f
Liangjin Huang <liangjin.huang@dena.jp>
2023-01-30 18:18:56 +0900

    stop recreating sa

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "google_project_iam_member" "terraform_sa_role" {
   ])
   project = local.project_id
   role    = each.value
-  member  = "serviceAccount:${google_service_account.terraform-sa.email}"
+  member  = "serviceAccount:${data.google_service_account.terraform-sa.email}"
 }
 
 # Workload Identity Pool 設定
@@ -81,18 +81,14 @@ resource "google_iam_workload_identity_pool_provider" "my-wif-provider" {
 }
 
 # GitHub Actions が借用するサービスアカウント
-resource "google_service_account" "terraform-sa" {
-  account_id   = "terraform-github"
-  description  = "A Terraform Service Account for GitHub Actions"
-  disabled     = false
-  display_name = "Terraform SA for GitHub Actions"
-  project      = local.project_id
+data "google_service_account" "terraform-sa" {
+  account_id = local.terraform_service_account
 }
 
 
 # サービスアカウントの IAM Policy 設定と GitHub リポジトリの指定
 resource "google_service_account_iam_member" "terraform-sa" {
-    service_account_id = google_service_account.terraform-sa.id
+    service_account_id = data.google_service_account.terraform-sa.id
     role               = "roles/iam.workloadIdentityUser"
     member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.my-wif-pool.name}/attribute.repository/${local.github_repository}"
 }
